e5cd653462913d3337467072aa400d3d502cfadb
mebigti <69617880+mebigti@users.noreply.github.com>
2024-03-23 05:38:23 +0000

    20240323 commit by guava

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def main():
     with st.sidebar:
         model_selection = st.selectbox(
             "Choose the language model",
-            ("gpt-3.5-turbo", "gpt-4-turbo-preview"),
+            ("gpt-3.5-turbo", "gpt-4-turbo-preview", "ft:gpt-3.5-turbo-1106:guava:bigtime:91yARKVz"),
             key="model_selection"
         )
         uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx', 'pptx'], accept_multiple_files=True)
